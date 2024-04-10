47c2cc68a9c70ccc3ab3b52aef98bac1a9f2d7da
tamandrew <andrew.c.tam@stonybrook.edu>
2024-04-10 17:59:49 -0400

    Clean up after merge

--- market/lib-proto/build.rs
+++ market/lib-proto/build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //tonic_build::compile_protos("market/market.proto")?;
+    //tonic_build::compile_protos("../../proto/market.proto")?;
     tonic_build::configure()
         .type_attribute("User", "#[derive(serde::Deserialize, serde::Serialize)]")
-        .compile(&["market/market.proto"], &["market"])?;
+        .compile(&["../../proto/market.proto"], &["../../proto/"])?;
     Ok(())
 }
